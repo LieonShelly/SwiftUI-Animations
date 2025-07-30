eff57e1d5f4e8de22a14a0368c3e00aabf13f10c
lieon <lieoncx@gmail.com>
2025-07-30 18:33:25 +0800

    Add segment tab

--- SwiftUI-Animations/SegmentTab/View/TabBarView.swift
+++ SwiftUI-Animations/SegmentTab/View/TabBarView.swift
@@ -38,37 +38,3 @@ struct TabBarView: View {
         }
     }
 }
-
-import SwiftUI
-
-struct TabBarItemView: View {
-    let topic: Topic
-    let isActive: Bool
-    var activeHighlightColor: Color = .purple
-    let animationNameSpace: Namespace.ID
-    let height: CGFloat
-    let onSelect: () -> Void
-    
-    var body: some View {
-        VStack(spacing: .zero) {
-            Text(topic.name.capitalized)
-                .font(.system(size: 16, weight: .semibold))
-                .foregroundStyle(isActive ? Color.primary : Color.gray)
-            Spacer()
-        }
-        .frame(height: height)
-        .overlay(alignment: .bottom) {
-            if isActive {
-                activeHighlightColor
-                    .frame(height: 3)
-                    .clipShape(.capsule)
-                    .matchedGeometryEffect(id: "ACTIVE_TAV", in: animationNameSpace)
-            }
-        }
-        .contentShape(.rect)
-        .onTapGesture {
-            guard !isActive else { return }
-            onSelect()
-        }
-    }
-}
